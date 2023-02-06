d4409675067c111ab9273afa647d9e790c5c092f
Pongchaiwat <p.pongchaiwat@gmail.com>
2023-02-06 13:53:51 -0500

    jsTimer

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ function countDown(time) {
     let timer = setInterval(function() {
         time--;
         if(time<=0){
-        clearInterval(timer);
-        console.log("DONE");
-    }
-    else{
-        console.log(time);
-    }
+            clearInterval(timer);
+            console.log("DONE");
+        }
+        else {
+            console.log(time);
+        }
     }
     , 1000);
 }
