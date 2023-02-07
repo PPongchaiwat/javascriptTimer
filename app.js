71ff3b2b5acac38d306ea26e74281d4945d437ab
Pongchaiwat <p.pongchaiwat@gmail.com>
2023-02-06 22:13:06 -0500

    second question

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ function countDown(time) {
     }
     , 1000);
 }
+
+function randomGame(){
+    let num;
+    let time = 0;
+    let timer = setInterval(function(){
+      num = Math.random();
+      time++
+      if(num > 0.75) {
+        clearInterval(timer);
+        console.log("It took " + time + " tries.");
+      }
+    },1000)
+  }
